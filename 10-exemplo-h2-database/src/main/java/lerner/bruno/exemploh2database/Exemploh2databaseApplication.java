ae1d7ae9449db0ad9c8e2d5521a3370a0630f218
Bruno-Lerner <brunolerner1000@gmail.com>
2020-04-22 19:05:55 -0300

    Versão 1.0

---
+++ 10-exemplo-h2-database/src/main/java/lerner/bruno/exemploh2database/Exemploh2databaseApplication.java
@@ -0,0 +1,19 @@
+package lerner.bruno.exemploh2database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Bruno Lerner
+ * @version 1.0
+ * @since 20/04/2020 10:30
+ */
+@SpringBootApplication
+public class Exemploh2databaseApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Exemploh2databaseApplication.class, args);
+	}
+
+}
---
+++ 10-exemplo-h2-database/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into funcionario(id_funcionario, nome, salario, rg)
+    values(null, 'Bruno Lerner', 2000.89, 1231231231);
+
+insert into funcionario(id_funcionario, nome, salario, rg)
+     values(null, 'Carina Lerner', 200.00, 1231231231);
